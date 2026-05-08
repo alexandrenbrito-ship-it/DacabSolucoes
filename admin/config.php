c5d76d282a8a16343e3520aee7d8812b05678df7
alexandrenbrito-ship-it <alexandrenbrito@gmail.com>
2026-05-07 22:23:06 -0300

    Delete admin directory

--- admin/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../config.php';
-Auth::requireAdmin();
-$pageTitle = 'Configurações do Sistema';
-include __DIR__ . '/header.php';
-?>
-<div class="alert alert-info">Página de configurações em desenvolvimento.</div>
-<?php include __DIR__ . '/footer.php'; ?>
